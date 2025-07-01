3388d4977dbbf12101ca1d368eef47a2a37fd5d0
Victor Varela <victor398@gmail.com>
2025-07-01 12:42:43 -0300

    feat: add form styles

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,17 +1,18 @@
+import styles from "./Form.module.css";
 import { countries } from "../../data";
 
 const Form = () => {
   return (
     <>
-      <form>
+      <form className={styles.form}>
         <div>
           <label htmlFor="city">Ciudad:</label>
           <input id="city" type="text" name="city" placeholder="Ciudad" />
         </div>
         <div>
-          <label htmlFor="contries">Pais:</label>
-          <select >
-            <option id="countries">--Seleccione un pais--</option>
+          <label htmlFor="countries">Pais:</label>
+          <select id="countries">
+            <option>--Seleccione un pais--</option>
             {countries.map(country => (
               <option value={country.code} key={country.code}>
                 {country.name}
@@ -19,7 +20,9 @@ const Form = () => {
             ))}
           </select>
         </div>
-        <input type="submit" value='Consultar Clima' />
+        <div>
+          <input type="submit" value="Consultar Clima" />
+        </div>
       </form>
     </>
   );
--- src/data/index.ts
+++ src/data/index.ts
@@ -5,5 +5,6 @@ export const countries = [
   { code: 'CO', name: 'Colombia' },
   { code: 'CR', name: 'Costa Rica' },
   { code: 'ES', name: 'España' },
-  { code: 'PE', name: 'Perú' }
+  { code: 'PE', name: 'Perú' },
+  { code: 'UY', name: 'Uruguay' }
 ]
